c81f07514fa196cc263179e7de630ee8573c8463
= <=>
2023-07-25 18:49:59 +0530

    Made smalll modification

--- script.py
+++ script.py
@@ -4,7 +4,6 @@ print(sys.version)
 print(sys.executable)
 
 def greet(who_to_greet):
-    """Function printing python version."""
     greeting = "Hello, {}".format(who_to_greet)    
     return greeting
 
